4d1179f1819b8798cbb5171d081abddc3cc873b4
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-10-20 22:34:11 -0500

    add routing

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const Layout = (props) => {
     return (
         <>
             <h1>Header</h1>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="about">About</Link>
+            </nav>
             <main>
              {props.children}
             </main>
