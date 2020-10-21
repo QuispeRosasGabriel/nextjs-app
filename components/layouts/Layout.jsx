d754c48f1eddc1c407adf1a88c32e5fee69f2bbc
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-10-21 11:22:44 -0500

    layout

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -49,6 +49,7 @@ const Layout = (props) => {
                 <link rel="stylesheet" 
                     href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" 
                     crossorigin="anonymous" />
+                <link href="/static/css/app.css" rel="stylesheet" />
             </Head>
             <Header />
             <main>
