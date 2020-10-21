d10544b48db703c27b1e7d7dcb02d59eaead1dd7
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-10-20 22:39:19 -0500

    add header

---
+++ components/layouts/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Buscar from '../ui/Buscar'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <div>
+                    <p>P</p>
+                    <Buscar />
+                </div> 
+                <div>
+                
+                </div>   
+            </div>
+        </header>
+    )
+}
+
+export default Header
