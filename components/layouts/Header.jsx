a96a324ffd0f3f04b52d7e0c02cee18262eed21f
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-10-21 12:06:31 -0500

    styled component

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -2,13 +2,41 @@ import React from 'react'
 import Search from '../ui/Search'
 import Navbar from './Navbar'
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const ContenedorHeader = styled.div`
+    max-width:1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media(min-width:768px) {
+        display: flex;
+        justify-content: space-betwee;
+    }    
+`
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    margin-right: 2rem;
+`
 
 const Header = () => {
     return (
-        <header>
-            <div>
+        <header
+            css={
+                css`
+                border-bottom: 2px solid var(--gris3);
+                padding: 1rem 0;    
+            `}            
+        >
+            <ContenedorHeader>
                 <div>
-                    <p>P</p>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
                     <Search />
                     <Navbar  />
                 </div> 
@@ -18,7 +46,7 @@ const Header = () => {
                     <Link href="/">Login</Link>
                     <Link href="/">Crear cuenta</Link>
                 </div>   
-            </div>
+            </ContenedorHeader>
         </header>
     )
 }
