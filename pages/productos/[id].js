ba6e66b0a38a28accd8683630acde6f9e28122ea
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2020-10-26 21:14:13 -0500

    controlando captura con useeffect

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,6 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Producto = () => {
+
+    // Router para obtener el id actual
+    const router = useRouter();
+    const {query: {id}} = router;
+
+    useEffect(() => {
+        if(id) {
+            console.log();
+        }
+    }, [id])
+
     return ( 
-        <h1>Desde producto</h1>
+    <h1>Desde producto xd {id}</h1>
      );
 }
  
